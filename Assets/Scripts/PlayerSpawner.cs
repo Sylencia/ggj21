5a22e3a3cfea030a87a45e11e13f13cacdd8c572
betterwithlasers <fa_767@hotmail.com>
2021-01-31 13:54:21 +1100

    Fixed splitscreen (mostly)

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+//using UnityEngine.InputSystem;
 
 public class PlayerSpawner : MonoBehaviour
 {
@@ -25,7 +26,7 @@ public class PlayerSpawner : MonoBehaviour
         int oldMask1 = c1.cullingMask;
         c1.cullingMask = oldMask1 & ~(1 << layer2);     //make layer2 invisible for this camera
 
-
+  
         GameObject P2 = Instantiate(player) as GameObject;
         P2.transform.position = new Vector3(5, 2, 5);
         Camera c2 = P2.GetComponentInChildren<Camera>();
@@ -36,6 +37,35 @@ public class PlayerSpawner : MonoBehaviour
 
         int oldMask2 = c2.cullingMask;
         c2.cullingMask = oldMask2 & ~(1 << layer1);     //make layer1 invisible for this camera
+
+
+
+        /*
+        PlayerInput i1 = P1.GetComponent<PlayerInput>();
+
+        
+//        i1.SwitchCurrentActionMap(i1.actions.FindActionMap("p1actions").ToString());
+       // i1.currentActionMap = i1.actions.FindActionMap("gameplayActions");
+
+
+        CinemachineInputProvider cip1 = P1.GetComponentInChildren<CinemachineInputProvider>();
+        InputActionReference ir1 = InputActionReference.Create(i1.currentActionMap.FindAction("Look"));
+
+        cip1.XYAxis = ir1;
+
+
+        PlayerInput i2 = P2.GetComponent<PlayerInput>();
+        //i1.SwitchCurrentActionMap("p2actions");
+        //i2.SwitchCurrentActionMap(i1.actions.FindActionMap("p2actions").id.ToString());
+        i2.currentActionMap = i1.actions.FindActionMap("gameplayActions");
+
+        CinemachineInputProvider cip2 = P2.GetComponentInChildren<CinemachineInputProvider>();
+        InputActionReference ir2 = InputActionReference.Create(i2.currentActionMap.FindAction("Look"));
+
+        cip2.XYAxis = ir2;
+
+        //i1.currentActionMap.FindAction("Look").ReadValue<Vector2>();
+        */
     }
 
     // Update is called once per frame
